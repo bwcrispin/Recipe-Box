11210a7ea46d904454176de7bdc00144dee89011
jessicawright <wright.983@gmail.com>
2019-04-01 10:38:36 -0400

    added a few images initializer

--- recipe-box-front-end/dist/app.c3f9f951.js
+++ recipe-box-front-end/dist/app.c3f9f951.js
@@ -408,7 +408,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60029" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63131" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
